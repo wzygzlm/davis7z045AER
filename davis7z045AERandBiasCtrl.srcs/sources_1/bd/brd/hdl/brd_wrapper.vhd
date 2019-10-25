b87d5348a73b915653efb29451996dded94e8ef2
Min Liu <wzygzlm@gmail.com>
2019-10-25 09:57:09 +0200

    A version works only if the HLS ip from eventstrem to frame strame works.

--- davis7z045AERandBiasCtrl.srcs/sources_1/bd/brd/hdl/brd_wrapper.vhd
+++ davis7z045AERandBiasCtrl.srcs/sources_1/bd/brd/hdl/brd_wrapper.vhd
@@ -1,7 +1,7 @@
 --Copyright 1986-2018 Xilinx, Inc. All Rights Reserved.
 ----------------------------------------------------------------------------------
 --Tool Version: Vivado v.2018.1 (win64) Build 2188600 Wed Apr  4 18:40:38 MDT 2018
---Date        : Thu Oct 24 15:25:30 2019
+--Date        : Fri Oct 25 09:13:44 2019
 --Host        : DESKTOP-3TNSMFC running 64-bit major release  (build 9200)
 --Command     : generate_target brd_wrapper.bd
 --Design      : brd_wrapper
